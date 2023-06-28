7d40ca2b35760f84a6bda8b110950765f56e299a
Josiah Raymundo <josiah.o.r@gmail.com>
2023-06-28 19:18:32 +0800

    add another button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("clicked!", e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("Clicked Again! :" + name, e.target);
+    }
     return ( <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={(e) => handleClickAgain('marion', e)}>Click Me Again</button>
     </div> );
 }
  
